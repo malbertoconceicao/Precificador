ff6c8406ca47f3ff6b5ddf4fe1c222c8192433e5
Marcos Alberto <m.albertoconceicao@gmail.com>
2022-07-12 09:47:30 -0300

    menuVoltar

--- Menus.cs
+++ Menus.cs
@@ -14,6 +14,23 @@ namespace Precificador
             Console.WriteLine("Digite a porcentagem desejada ex 50% = 50");
             var porcentagem = int.Parse(Console.ReadLine());
             Calculos.Calcula(valor, porcentagem);
+            MenuVoltar();
+        }
+        public static void MenuVoltar()
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+            Console.WriteLine("1 - Deseja voltar?");
+            Console.WriteLine("2 - Sair");
+            var opcao = int.Parse(Console.ReadLine());
+
+            switch(opcao)
+            {
+                case 1:MenuPrincipal(); break;
+                case 2: System.Environment.Exit(0);break;
+                default:MenuPrincipal();break;
+            }
         }
     }
 }
